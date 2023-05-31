da42f22a59aee6c731684fbeaba6a8c119fb92c9
saimon siddique <saimonsiddiquee@gmail.com>
2023-05-31 12:06:13 +0600

    Make skillbubble component

---
+++ client/src/components/StudentInfo/SkillBubbleChart.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+// @ts-ignore
+
+function SkillBubbleChart() {
+  return (
+    <div>
+      <h1>hello</h1>
+    </div>
+  );
+}
+export default SkillBubbleChart;
